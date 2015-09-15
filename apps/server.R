61267fa2bcb1cb455a755caefdaf08ec96ea1224
unknown <pozypakya@gmail.com>
2015-09-16 05:39:37 +0800

    test1

--- apps/server.R
+++ apps/server.R
@@ -12,6 +12,24 @@ shinyServer(function(input, output, session) {
     cat(input$voltage, '\n', file = logfilename, append = TRUE)
   })
   
+  w <- function(voltage,ohms) {
+    result <- (voltage * voltage) / ohms
+    return(result)
+  }
+  
+  r <- function(watt) {
+    if ( watt >= 2 & watt <3.4 ) {
+      return <- "<b><font color=\"blue\">Power level maybe to low to produce a string vapor</font></b>"
+    }
+    else if ( watt >= 3.4 & watt <= 8.5 ) {
+      return <- "<b><font color=\"green\" >Best vapor and Cartomizer / Atomizer Performance</font></b>"
+    }
+    else if ( watt > 8.5  ) {
+      return <- "<b><font color=\"red\"> Danger ! Power may melt wire </font></b>"
+    }
+  }
+  
+  
   
   # When the client ends the session, suspend the observer.
   # Otherwise, the observer could keep running after the client
@@ -25,11 +43,23 @@ shinyServer(function(input, output, session) {
   
 
   output$voltage <- renderText({
-    paste0("The value of voltage is: ", input$voltage)
+    paste0("The Voltage is: ", input$voltage)
   })
   
   output$ohms <- renderText({
-    paste0("The value of voltage is: ", input$ohms)
+    paste0("The Resistance is: ", input$ohms)
+  })
+  
+  output$watt <- renderText({
+    paste0("The Watt is: ", w(input$voltage,input$ohms)   )
+  })
+  
+  output$recommendation <- renderText({
+    paste0(r(w(input$voltage,input$ohms)) )
+  })
+  
+  output$color <- renderText({
+    paste0(HTML(r(w(input$voltage,input$ohms))) )
   })
   
 })
--- apps/ui.R
+++ apps/ui.R
@@ -1,21 +1,24 @@
 library(shiny)
 shinyUI(fluidPage(
-  titlePanel("Observer demo"),
+  titlePanel("Safe Vaping Power Analyzer"),
   fluidRow(
     column(4, wellPanel(
       sliderInput("voltage", "Voltage:",
-                  min = 3.2, max = 6.1, value = 4.5, step= 0.1),
+                  min = 3.2, max = 6.1, value = 3.9, step= 0.1),
       sliderInput("ohms", "Ohms:",
-                  min = 3.2, max = 6.1, value = 4.5, step= 0.1)
+                  min = 1.4, max = 5.1, value = 3.0, step= 0.1)
     )),
     column(8,
            verbatimTextOutput("voltage"),
            verbatimTextOutput("ohms"),
+           verbatimTextOutput("watt"),
+           verbatimTextOutput("recommendation"),
+           uiOutput("color"),
            br(),
-           br(),
-           p("In this example, what's visible in the client isn't",
-             "what's interesting. The server is writing to a log",
-             "file each time the slider value changes.")
+           p("Resistance will also play a role in battery life. Current is what determines how 
+             long your battery can be used. When you lower the resistance with the same Voltage, 
+             you increase the current. Resistance is measured in Ohms, Energy is measured in Volts, 
+             Power is measured in Watts, and Current(I) is measured in Amps.")
     )
   )
 ))
